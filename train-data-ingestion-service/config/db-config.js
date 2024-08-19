545181ea18d0879a70592fa8cf836319fcb1223f
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2024-08-20 00:41:51 +0530

    develop db config and controller

---
+++ train-data-ingestion-service/config/db-config.js
@@ -0,0 +1,27 @@
+const MongoClient = require("mongodb").MongoClient;
+require("dotenv").config();
+
+const url = process.env.MONGO_URI;
+const dbName = process.env.DB_NAME;
+
+const client = new MongoClient(url, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function connectToDatabase() {
+  await client.connect();
+  const db = client.db(dbName);
+  const collection = db.collection("trainData");
+
+  // Create a TTL index on the timestamp field
+  await collection.createIndex(
+    { timestamp: 1 },
+    { expireAfterSeconds: 90 * 24 * 60 * 60 }
+  ); // 90 days
+  console.log("TTL index created on 'timestamp' field");
+
+  return db;
+}
+
+module.exports = { connectToDatabase };
