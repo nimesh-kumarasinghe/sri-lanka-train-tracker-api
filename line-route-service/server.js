14ca5347902f5edf8b3811716de8b154cbdf9d9f
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2024-08-23 01:27:12 +0530

    implement server logs

--- line-route-service/server.js
+++ line-route-service/server.js
@@ -7,6 +7,8 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT;
 
+const log = require("./utils/logger");
+
 app.use(express.json());
 app.use(morgan("combined"));
 
@@ -15,5 +17,6 @@ app.use("/api", lineRoutes, routeRoutes);
 connectToDatabase();
 
 app.listen(PORT, () => {
+  log(`Engine Train Service running on port ${PORT}`);
   console.log(`Line Route Service running on port ${PORT}`);
 });
