2d5360cf7407ea17627e9baaeabc06d998cfdd46
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2024-08-23 00:03:41 +0530

    updated routes

--- engine-train-service/routes/trainRoutes.js
+++ engine-train-service/routes/trainRoutes.js
@@ -5,7 +5,7 @@ const trainController = require("../controllers/trainController");
 router.get("/trains", trainController.getAllTrains);
 router.get("/trains/:id", trainController.getTrainById);
 router.post("/trains", trainController.createTrain);
-router.post("/trains/:id", trainController.updateTrain);
+router.put("/trains/:id", trainController.updateTrain);
 router.delete("/trains/:id", trainController.deleteTrain);
 
 module.exports = router;
