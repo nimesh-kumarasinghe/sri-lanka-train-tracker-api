d7e71589eb1edc464a68c2a7b51cf6e431661b48
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2024-08-23 12:50:14 +0530

    database configuration and added server file

---
+++ trip-schedule-station-service/server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const morgan = require("morgan");
+const { connectToDatabase } = require("./config/dbConfig");
+require("dotenv").config();
+const app = express();
+const PORT = process.env.PORT;
+
+app.use(express.json());
+app.use(morgan("combined"));
+
+connectToDatabase();
+
+app.listen(PORT, () => {
+  console.log(`Line Route Service running on port ${PORT}`);
+});
