13ce9c033bb845bcc51cfd2ab4a8408b203c9d5d
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2024-08-23 23:33:28 +0530

    integrating user public and private routes

--- api-gateway/routes/apiGatewayRoutes.js
+++ api-gateway/routes/apiGatewayRoutes.js
@@ -3,13 +3,16 @@ const router = express.Router();
 const authMiddleware = require("../middlewares/authMiddleware");
 
 // Public Routes
-const userRoutes = require("./userRoutes");
-router.use("/users", userRoutes);
+const userRoutes = require("./userPublicRoutes");
+router.use("/user", userRoutes);
 
 // Protected Routes
+const userPrivateRoutes = require("./userPrivateRoutes");
 const trainRoutes = require("./trainRoutes");
 // const tripScheduleRoutes = require("./tripScheduleRoutes");
 // const lineRouteRoutes = require("./lineRouteRoutes");
+
+router.use("/users", authMiddleware.verifyToken, userPrivateRoutes);
 router.use("/trains", authMiddleware.verifyToken, trainRoutes);
 // router.use("/trip-schedule", authMiddleware.verifyToken, tripScheduleRoutes);
 // router.use("/line-route", authMiddleware.verifyToken, lineRouteRoutes);
--- api-gateway/routes/userPublicRoutes.js
+++ api-gateway/routes/userPublicRoutes.js
@@ -5,8 +5,4 @@ const apiGatewayController = require("../controllers/apiGatewayController");
 router.post("/register", apiGatewayController.register);
 router.post("/login", apiGatewayController.login);
 
-router.get("/:id", apiGatewayController.getUser);
-router.put("/:id", apiGatewayController.updateUser);
-router.delete("/:id", apiGatewayController.deleteUser);
-
 module.exports = router;
