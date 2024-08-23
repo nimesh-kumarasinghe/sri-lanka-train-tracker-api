02607a2ce78141dda29c3e6f7c1b743b607aad1b
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2024-08-23 20:22:47 +0530

    logs implemented

--- user-service/server.js
+++ user-service/server.js
@@ -2,12 +2,16 @@ const express = require("express");
 const userRoutes = require("./routes/userRoutes");
 const app = express();
 require("dotenv").config();
+const morgan = require("morgan");
 
 app.use(express.json());
 app.use("/api", userRoutes);
+app.use(morgan("combined"));
 
 const PORT = process.env.PORT;
+const log = require("./utils/logger");
 
 app.listen(PORT, () => {
+  log(`Engine Train Service running on port ${PORT}`);
   console.log(`User Auth Service running on port ${PORT}`);
 });
