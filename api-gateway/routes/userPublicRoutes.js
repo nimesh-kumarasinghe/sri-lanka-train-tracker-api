072203379ed844ab42a7cfb1acb0d7cef052c136
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2024-08-24 23:01:14 +0530

    update private routes

--- api-gateway/routes/userPublicRoutes.js
+++ api-gateway/routes/userPublicRoutes.js
@@ -2,7 +2,6 @@ const express = require("express");
 const router = express.Router();
 const apiGatewayController = require("../controllers/apiGatewayController");
 
-router.post("/register", apiGatewayController.register);
 router.post("/login", apiGatewayController.login);
 
 module.exports = router;
