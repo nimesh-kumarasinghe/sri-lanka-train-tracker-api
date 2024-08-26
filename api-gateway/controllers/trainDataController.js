001906cd4a5d45009ed3b5fd3be2c673e3fe3a42
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2024-08-26 17:01:05 +0530

    updating

--- api-gateway/controllers/trainDataController.js
+++ api-gateway/controllers/trainDataController.js
@@ -1,17 +1,7 @@
 const axios = require("axios");
 const config = require("../config/gatewayConfig");
 
-// const getAllTrainData = async (req, res) => {
-//   try {
-//     const response = await axios.get(
-//       `${config.locationServiceUrl}/v1/trains/summary`
-//     );
-//     res.status(200).json(response.data);
-//   } catch (error) {
-//     res.status(error.response?.status || 500).json({ message: error.message });
-//   }
-// };
-
+// Get all train data
 const getAllTrainData = async (req, res) => {
   try {
     // Extract pagination parameters from the query string
@@ -31,34 +21,7 @@ const getAllTrainData = async (req, res) => {
   }
 };
 
-// // Get train data by start and end stations
-// const getTrainDataByStations = async (req, res) => {
-//   const { startStationName, endStationName } = req.query;
-
-//   // Validate the presence of required query parameters
-//   if (!startStationName || !endStationName) {
-//     return res
-//       .status(400)
-//       .json({ error: "startStationName and endStationName are required" });
-//   }
-
-//   try {
-//     const response = await axios.get(
-//       `${config.locationServiceUrl}/v1/trains/summary/by-stations`,
-//       {
-//         params: {
-//           startStationName,
-//           endStationName,
-//         },
-//       }
-//     );
-//     res.status(200).json(response.data);
-//   } catch (error) {
-//     res.status(error.response?.status || 500).json({ message: error.message });
-//   }
-// };
-
-// // Get train data by start and end stations
+// Get train data by start and end stations
 const getTrainDataByStations = async (req, res) => {
   const { startStationName, endStationName, page = 1, limit = 10 } = req.query;
 
--- train-location-service/controllers/locationController.js
+++ train-location-service/controllers/locationController.js
@@ -12,17 +12,7 @@ const updateLocation = async (req, res) => {
   }
 };
 
-// // get all data from location collection
-// const getAllTrainData = async (req, res) => {
-//   try {
-//     const locations = await Location.find();
-//     res.status(200).json(locations);
-//   } catch (err) {
-//     console.error("Error in getAllLocations controller:", err);
-//     res.status(500).json({ error: "Failed to retrieve location data" });
-//   }
-// };
-
+//  get all data from location collection
 const getAllTrainData = async (req, res) => {
   try {
     const page = parseInt(req.query.page);
@@ -45,35 +35,7 @@ const getAllTrainData = async (req, res) => {
   }
 };
 
-// // get trains by start and end stations
-// const getTrainDataByStations = async (req, res) => {
-//   const { startStationName, endStationName } = req.query;
-
-//   if (!startStationName || !endStationName) {
-//     return res
-//       .status(400)
-//       .json({ error: "startStationName and endStationName are required" });
-//   }
-
-//   try {
-//     // Query MongoDB to find documents matching the start and end station names
-//     const locations = await Location.find({
-//       startStationName: { $regex: new RegExp(`^${startStationName}$`, "i") },
-//       endStationName: { $regex: new RegExp(`^${endStationName}$`, "i") },
-//     });
-
-//     if (locations.length === 0) {
-//       return res
-//         .status(404)
-//         .json({ message: "No data found for the provided station names" });
-//     }
-
-//     res.status(200).json(locations);
-//   } catch (err) {
-//     console.error("Error in getTrainDataByStations controller:", err);
-//     res.status(500).json({ error: "Failed to retrieve location data" });
-//   }
-// };
+//  get trains by start and end stations
 
 const getTrainDataByStations = async (req, res) => {
   const { startStationName, endStationName, page = 1, limit = 10 } = req.query;
