6bb67da321eb2d2b76db0481474bd68e3f1ef967
Nimesh Kumarasinghe <121744102+nimesh-kumarasinghe@users.noreply.github.com>
2024-08-29 16:23:17 +0530

    updates

--- api-gateway/middlewares/authMiddleware.js
+++ api-gateway/middlewares/authMiddleware.js
@@ -6,12 +6,12 @@ const verifyToken = (req, res, next) => {
   const token = req.headers["authorization"]?.split(" ")[1];
 
   if (!token) {
-    return res.status(401).json({ error: "No token provided" });
+    return res.status(401).json({ message: "No token provided" });
   }
 
   jwt.verify(token, config.secret, (err, decoded) => {
     if (err) {
-      return res.status(403).json({ error: "Failed to authenticate token" });
+      return res.status(403).json({ message: "Failed to authenticate token" });
     }
 
     req.user = decoded; // Save decoded info for use in other routes
